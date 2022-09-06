44fbd87373707dbdf46aadab97829cc9a22bdb6d
15ariko <110599821+15ariko@users.noreply.github.com>
2022-09-06 10:37:09 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -12,4 +12,14 @@ let matrix_2 = [
     [1, 0, 1]
 ]; 
 
-// Write code here. 
+let sum = 0;
+for (let i = 0; i < matrix_2.length; i++) {
+  let row = matrix_2[i];
+  for (let j = 0; j < row.length; j++) {
+    const matrx = matrix_2[i][j];
+    if (matrx === 1) {
+      sum += matrix_1[i][j];
+    }
+  }
+}
+console.log(sum)
