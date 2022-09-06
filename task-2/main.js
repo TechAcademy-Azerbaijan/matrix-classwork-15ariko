d9a21db0a4cdecbe3341c3e46346cadc039f1f7f
15ariko <110599821+15ariko@users.noreply.github.com>
2022-09-06 09:57:21 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,4 +1,21 @@
 // Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
 
-// Write code here. 
+let arr = [];
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  let exist = false;
+
+  for (let j = i + 1; j < array.length; j++) {
+    const element1 = array[j];
+    if (element == element1) {
+      exist = true;
+      break;
+    }
+  }
+  if (exist === false) {
+    arr.push(array[i]);
+  }
+}
+console.log(arr.length);
